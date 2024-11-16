5d9abae9538a267c980942a89f38e661678b2845
Abdulkadir <qansax30@hotmail.com>
2024-11-16 21:16:00 +0100

    Add project files.

---
+++ Debugging1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Abdulkadir";
+            int age; //0
+            age = 23;
+
+            if (1 = = 1)
+                Console.WriteLine("True");
+            PrintDetails(names, age);
+            string message = ReadFromConsole("Enter anything: ");
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+
+        static string ReadFromConsole(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+
+    static void PrintDetails(string name, int age)
+    {
+        Console.WriteLine($"{name} - {age}");
+        Console.WriteLine($"{name} - {age}");
+        Console.WriteLine($"{name} - {age}");
+        Console.WriteLine($"{name} - {age}");
+        Console.WriteLine($"{name} - {age}");
+
+    }
+} 
+}
